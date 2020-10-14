ca9cac3e6eb091ec29cb3d1a5aa49d1864228501
SimonCK666 <SimonYang223@163.com>
2020-10-14 21:19:17 +0800

    'Lab2Part2'

--- code/Lab2_1930026144_Simon/Lab2Part2/Question3/src/TestPalindrome.java
+++ code/Lab2_1930026144_Simon/Lab2Part2/Question3/src/TestPalindrome.java
@@ -7,31 +7,40 @@ public class TestPalindrome {
         Scanner in = new Scanner(System.in);
         // Input a string
         String inStr = in.nextLine();
-        // change all the letters into lower case
-        String lowStr = inStr.toLowerCase();
+        
         
         // close the Scanner
         in.close();
         
-        String lowStr_withoutSpaces = lowStr.replaceAll(" +", "");
+        String str = "";
         // Test
-        // System.out.println("The without spaces is " + lowStr_withoutSpaces);
+        // System.out.println(inStr.length());
+        
+        // get rid of all the spacess
+        for (int i = 0; i < inStr.length(); i++) {
+            if (Character.isLetter(inStr.charAt(i))) {
+                str += inStr.charAt(i);
+            }
+        }
+
+        // change all the letters into lower case
+        String lowStr = str.toLowerCase();
 
         // forward & backward indexes
-        int fIdx = 0, bIdx = inStr.length() - 1;
+        int fIdx = 0, bIdx = lowStr.length() - 1;
         char fChar, bChar;
         while (fIdx < bIdx) {
-            fChar = lowStr_withoutSpaces.charAt(fIdx);
-            bChar = lowStr_withoutSpaces.charAt(bIdx);
-            // judge whether it is palindrome
+            fChar = lowStr.charAt(fIdx);
+            bChar = lowStr.charAt(bIdx);
+            // judge whether it is palindromic sentences
             if (fChar != bChar) {
-                System.out.println("\"" + inStr + "\" is not a palindrome");
+                System.out.println("\"" + inStr + "\" is not a palindromic sentence");
                 return;
             } else {
                 fIdx++;
                 bIdx--;
             }
         }
-        System.out.println("\"" + inStr + "\" is a palindrome");
+        System.out.println("\"" + inStr + "\" is a palindromic sentence");
     }
 }
